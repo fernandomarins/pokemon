6cddb193edbec5b585754f4905ef456f989eebe2
f.augustomarins@gmail.com <fernandomarins@MacBook-Pro-from-Roberto-Borge.local>
2023-07-14 14:53:17 -0300

    Adding persistency

--- pokemon/ViewModel/PokemonListViewModel.swift
+++ pokemon/ViewModel/PokemonListViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 class PokemonListViewModel: ObservableObject {
     
@@ -13,12 +14,15 @@ class PokemonListViewModel: ObservableObject {
     @Published var pokemonNames = [String]()
     @Published var pokemonCellList = [PokemonCellModel]()
     
+    static let storedKey = "pokemon"
+    
     let service: APIServiceProtocol
     init(service: APIServiceProtocol = Service()) {
         self.service = service
     }
     
     func fetchPokemonList() async {
+        searchStoredData()
         do {
             try await service.fetchList { [weak self] result in
                 switch result {
@@ -39,7 +43,7 @@ class PokemonListViewModel: ObservableObject {
     func fetchPokemonURL() async {
         for name in pokemonNames {
             do {
-                try await service.fetchItem(name: name) { result in
+                try await service.fetchItem(name: name) { [weak self] result in
                     switch result {
                     case .success(let item):
                         if let url = URL(string: item.sprites.frontDefault),
@@ -57,6 +61,7 @@ class PokemonListViewModel: ObservableObject {
                             DispatchQueue.main.async { [weak self] in
                                 self?.pokemonCellList.append(pokemon)
                             }
+                            self?.storeData()
                         }
                     case let .failure(error):
                         print(error.localizedDescription)
@@ -67,10 +72,43 @@ class PokemonListViewModel: ObservableObject {
             }
         }
     }
+}
 
-    private func createListNames() {
+private extension PokemonListViewModel {
+    
+    func storeData() {
+        UserDefaults.standard.set(convertToData(array: pokemonCellList), forKey: PokemonListViewModel.storedKey)
+    }
+    
+    func searchStoredData() {
+        guard let arrayData = UserDefaults.standard.data(forKey: PokemonListViewModel.storedKey),
+              let convertedArray: [PokemonCellModel] = convertFromData(data: arrayData),
+              !convertedArray.isEmpty else {
+            return
+        }
+        
+        pokemonCellList = convertedArray
+    }
+    
+    func createListNames() {
         pokemonList.forEach {
             pokemonNames.append($0.name)
+        }
+    }
+    
+    func convertToData<T: Encodable>(array: [T]) -> Data? {
+        do {
+            let encoder = JSONEncoder()
+            let data = try? encoder.encode(array)
+            return data
+        }
+    }
+    
+    func convertFromData<T: Decodable>(data: Data) -> [T]? {
+        do {
+            let decoder = JSONDecoder()
+            let array = try? decoder.decode([T].self, from: data)
+            return array
         }
     }
 }
