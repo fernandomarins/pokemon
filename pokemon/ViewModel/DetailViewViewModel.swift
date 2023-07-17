4975e2ea99e7f47f827c5b86af70cac3300bba7f
Fernando Marins <f.augustomarins@gmail.com>
2023-07-17 20:45:21 -0300

    Removed persistency for DetailViewModel and also locked to portrait mode

--- pokemon/ViewModel/DetailViewViewModel.swift
+++ pokemon/ViewModel/DetailViewViewModel.swift
@@ -9,8 +9,6 @@ import Foundation
 import SwiftUI
 
 class DetailViewViewModel: ObservableObject {
-    static let storedKey = "pokemonDetail"
-
     @Published var pokemonDetailModel: PokemonDetailModel?
     
     let service: APIServiceProtocol
@@ -19,52 +17,28 @@ class DetailViewViewModel: ObservableObject {
     }
     
     func fetchPokemonType(id: Int?) async {
-        await searchStoredData { [weak self] in
-            if $0 {
-                guard let id = id else { return }
-                do {
-                    try await self?.service.fetchType(id: String(id)) { [weak self] result in
-                        switch result {
-                        case .success(let pokemon):
-                            DispatchQueue.main.async { [weak self] in
-                                let damageRelations = pokemon.damageRelations
-                                
-                                let weakAgainstList = damageRelations.noDamageTo.map(\.name) + damageRelations.doubleDamageFrom.map(\.name)
-                                let strongAgainstList = damageRelations.doubleDamageTo.map(\.name) + damageRelations.noDamageFrom.map(\.name)
-                                
-                                self?.pokemonDetailModel = PokemonDetailModel(
-                                    weakList: weakAgainstList,
-                                    strongList: strongAgainstList
-                                )
-                                self?.storeData()
-                            }
-                        case let .failure(error):
-                            print(error.localizedDescription)
-                        }
+        guard let id = id else { return }
+        do {
+            try await service.fetchType(id: String(id)) { [weak self] result in
+                switch result {
+                case .success(let pokemon):
+                    DispatchQueue.main.async { [weak self] in
+                        let damageRelations = pokemon.damageRelations
+                        
+                        let weakAgainstList = damageRelations.noDamageTo.map(\.name) + damageRelations.doubleDamageFrom.map(\.name)
+                        let strongAgainstList = damageRelations.doubleDamageTo.map(\.name) + damageRelations.noDamageFrom.map(\.name)
+                        
+                        self?.pokemonDetailModel = PokemonDetailModel(
+                            weakList: weakAgainstList,
+                            strongList: strongAgainstList
+                        )
                     }
-                } catch {
-                    print(error)
+                case let .failure(error):
+                    print(error.localizedDescription)
                 }
             }
+        } catch {
+            print(error)
         }
-    }
-}
-
-extension DetailViewViewModel {
-    func storeData() {
-        UserDefaults.standard.set(convertObjectToData(object: pokemonDetailModel), forKey: DetailViewViewModel.storedKey)
-    }
-    
-    func searchStoredData(continueHandler: @escaping (Bool) async -> Void) async {
-        guard let data = UserDefaults.standard.data(forKey: DetailViewViewModel.storedKey),
-              let convertedData: PokemonDetailModel = convertObjectFromData(data: data) else {
-            await continueHandler(true)
-            return
-        }
-        
-        DispatchQueue.main.async {  [weak self] in
-            self?.pokemonDetailModel = convertedData
-        }
-        await continueHandler(false)
     }
 }
