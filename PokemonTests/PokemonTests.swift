e9d9f0fac748e73945279de078f654e5c587a243
Fernando Marins <f.augustomarins@gmail.com>
2023-07-17 20:48:41 -0300

    Removed the UI unit tests bundle

--- PokemonTests/PokemonTests.swift
+++ PokemonTests/PokemonTests.swift
@@ -1,5 +1,5 @@
 //
-//  pokemonTests.swift
+//  PokemonTests.swift
 //  pokemonTests
 //
 //  Created by Fernando Marins on 7/12/23.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import pokemon
 
-final class pokemonTests: XCTestCase {
+final class PokemonTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
